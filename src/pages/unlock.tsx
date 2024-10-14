90783914cce326afa01b4dc40651d1b81470bd3b
Raghib Najmi <84832906+ragOP@users.noreply.github.com>
2024-10-15 05:03:39 +0530

    Update unlock.tsx

--- src/pages/unlock.tsx
+++ src/pages/unlock.tsx
@@ -596,7 +596,7 @@ const handleQuizN = () => {
         
         </div>
 
-<p>G2 Licensed Agent : Jessica Johanna Rodriguez</p>
+<p>G2 Licensed Agent : Gregory K. Teipelz</p>
         {/* <p>{zipCode} </p> */}
       </div>
       {/* <ToastContainer
